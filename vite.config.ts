8a41b47ca142afe143b2c302fbf00d9ad0f77ce1
yzuaoqian01 <yyuzaoqian@gmail.com>
2024-11-07 11:14:55 +0800

    master

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+import path from "path"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 3000
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
+})
